95c8316eed66eb682934e8cdcb15c4b5b9017564
David Shepherd <davidshepherd7@gmail.com>
2012-02-02 15:33:18 +0000

    Tidied up, added calculation of mean "error\, added plotting of "error"s

--- hybrid_boundary_element/quadrature_accuracy_test.sh
+++ hybrid_boundary_element/quadrature_accuracy_test.sh
@@ -3,28 +3,32 @@
 set -o errexit
 set -o nounset
 
-rm -f octave_script.m
-
-# # For a few different numbers of elements
-## for j in 5 10 50 250; do
-
+# The highest Gauss order used by the program
 max_i=29
 
-    # Build and run the program
+# Build and run the program
 make
-./hybrid_boundary_element_driver 5 5
+./hybrid_boundary_element_driver 50 50
 
-    # For each possible gauss order:
+# Now create the octave script:
+
+echo "format short e" > octave_script.m
+
+# For each possible gauss order:
 for (( i=2; i<=$max_i; i++ )); do
     # Load this matrix into octave
     echo "load \"results/boundary_matrix_$i\";" >> octave_script.m
 done
 
-    # Compare each matrix with the previous (using octave)
+# Compare each matrix with the previous (using octave)
 for (( i=2; i<=$max_i; i++ )); do
-    echo "results($i) = max(max(abs(boundary_matrix_$i - boundary_matrix_$max_i)));" >> octave_script.m
+    echo "max_diff($i) = max(max(abs(boundary_matrix_$i - boundary_matrix_$max_i)))" >> octave_script.m
+    echo "mean_diff($i) = mean(mean(abs(boundary_matrix_$i - boundary_matrix_$max_i)))" >> octave_script.m
 done
 
-octave --persist octave_script.m
+echo "semilogy(max_diff)" >> octave_script.m
+echo "figure" >> octave_script.m
+echo "semilogy(mean_diff)" >> octave_script.m
 
-# done
+# Run the script
+octave -q --persist octave_script.m
