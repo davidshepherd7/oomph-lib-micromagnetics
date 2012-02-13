d8a9024f8726ba133e096ea5c81fdfaa1c986921
David Shepherd <davidshepherd7@gmail.com>
2012-02-13 20:20:52 +0000

    Added plotting Gauss-Legendre curve to all plots for comparison (might not work if data file is not present...)
    Fixed to only plot from the second entry onwards, which is where the real
    entries start.

--- hybrid_boundary_element/quadrature_accuracy_test.sh
+++ hybrid_boundary_element/quadrature_accuracy_test.sh
@@ -6,9 +6,12 @@ set -o nounset
 # The highest Gauss order used by the program
 max_i=29
 
+# Number of elements to use in each direction
+n_ele=50
+
 # Build and run the program
 make
-./hybrid_boundary_element_driver 50 50
+./hybrid_boundary_element_driver $n_ele $n_ele
 
 # Now create the octave script:
 
@@ -26,9 +29,17 @@ for (( i=2; i<=$max_i; i++ )); do
     echo "mean_diff($i) = mean(mean(abs(boundary_matrix_$i - boundary_matrix_$max_i)))" >> octave_script.m
 done
 
-echo "semilogy(max_diff)" >> octave_script.m
+# load results from gauss-legendre quadrature
+echo "load \"results/gl_50x50_results\"" >> octave_script.m
+
+echo "hold on" >> octave_script.m
+echo "semilogy(max_diff(2:end))" >> octave_script.m
+echo "semilogy(gl_max_diff(2:end),'r')" >> octave_script.m
+
 echo "figure" >> octave_script.m
-echo "semilogy(mean_diff)" >> octave_script.m
+echo "hold on" >> octave_script.m
+echo "semilogy(mean_diff(2:end))" >> octave_script.m
+echo "semilogy(gl_mean_diff(2:end),'r')" >> octave_script.m
 
 # Run the script
 octave -q --persist octave_script.m
