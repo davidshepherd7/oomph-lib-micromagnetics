ba08f114b97551d20ef0680e82759b9f42943e1d
David Shepherd <davidshepherd7@gmail.com>
2012-02-14 12:45:20 +0000

    Removed dependence on Gaussian data - too annoying and useless.

--- hybrid_boundary_element/quadrature_accuracy_test.sh
+++ hybrid_boundary_element/quadrature_accuracy_test.sh
@@ -30,16 +30,16 @@ for (( i=2; i<=$max_i; i++ )); do
 done
 
 # load results from gauss-legendre quadrature
-echo "load \"results/gl_50x50_results\"" >> octave_script.m
+# echo "load \"results/gl_50x50_results\"" >> octave_script.m
 
 echo "hold on" >> octave_script.m
 echo "semilogy(max_diff(2:end))" >> octave_script.m
-echo "semilogy(gl_max_diff(2:end),'r')" >> octave_script.m
+# echo "semilogy(gl_max_diff(2:end),'r')" >> octave_script.m
 
 echo "figure" >> octave_script.m
 echo "hold on" >> octave_script.m
 echo "semilogy(mean_diff(2:end))" >> octave_script.m
-echo "semilogy(gl_mean_diff(2:end),'r')" >> octave_script.m
+# echo "semilogy(gl_mean_diff(2:end),'r')" >> octave_script.m
 
 # Run the script
 octave -q --persist octave_script.m
