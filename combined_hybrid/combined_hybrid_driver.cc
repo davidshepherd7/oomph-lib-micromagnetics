c3311ff1a78d979573ee3b451e72a02cb5e892ae
David Shepherd <davidshepherd7@gmail.com>
2012-03-26 22:14:01 +0100

    Skeleton for a driver using hybrid method in a full micromagnetic simulation.

---
+++ combined_hybrid/combined_hybrid_driver.cc
@@ -0,0 +1,28 @@
+#ifndef OOMPH_COMBINED_HYBRID_DRIVER_H
+#define OOMPH_COMBINED_HYBRID_DRIVER_H
+
+/*
+description of file goes here
+*/
+
+#include "generic.h"
+#include "../micromagnetics_boundary_element.h"
+#include "../micromagnetics_input.h"
+
+// Mesh
+#include "meshes/collapsible_channel_mesh.h"
+
+// My variable gauss quadrature header
+#include "../variable_order_quadrature.h"
+
+using namespace oomph;
+using namespace MathematicalConstants;
+
+namespace oomph
+{
+
+
+
+} // End of oomph namespace
+
+#endif
