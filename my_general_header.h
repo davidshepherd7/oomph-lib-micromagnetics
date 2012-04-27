66ed89cd9aa397f5d1d5f1ed2dd610710bb77ee7
David Shepherd <davidshepherd7@gmail.com>
2012-04-27 16:19:05 +0100

    A header file to hold debugging and easy output headers and/or functions.

---
+++ my_general_header.h
@@ -0,0 +1,19 @@
+#ifndef OOMPH_MY_GENERAL_HEADER_H
+#define OOMPH_MY_GENERAL_HEADER_H
+
+/*
+ A header for all my debug and output stuff.
+*/
+
+
+// Include the appropriate version of the pretty print header depending on if we
+// are using c++0x or not
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+  #include "prettyprint.hpp"
+#else
+  #include "prettyprint98.hpp"
+#endif
+
+#include <fenv.h>
+
+#endif
