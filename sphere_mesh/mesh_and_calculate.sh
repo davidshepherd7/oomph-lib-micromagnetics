57283883c0e71273bf02cecb881e636804247205
David Shepherd <davidshepherd7@gmail.com>
2012-05-01 22:17:37 +0100

    A shell script to automatically regenerate all mesh inputs, run tetgen on them , run the driver on the new tetgen mesh and finally plot everything using paraview.

---
+++ sphere_mesh/mesh_and_calculate.sh
@@ -0,0 +1,21 @@
+set -o errexit
+set -o nounset
+
+make
+
+# move old files to backup
+touch results/soln0.dat mesh.poly # make sure exist to avoid error in mv
+mv -f results/* mesh.* results_old/
+
+# Generate the input to tetgen to make a spherical mesh
+./generate_tetgen_sphere_input 1.0 3
+
+# Generate mesh, -Y = don't split facets, -q = make a good qualit
+tetgen -q1.0pa0.2 ./mesh.poly
+
+# Run the actual code
+./sphere_driver "mesh.1.node" "mesh.1.ele" "mesh.1.face"
+
+# paraview plot
+oomph-convert results/soln*.dat
+paraview --data="results/soln..vtu" --script="../paraview_vectors.py" &
