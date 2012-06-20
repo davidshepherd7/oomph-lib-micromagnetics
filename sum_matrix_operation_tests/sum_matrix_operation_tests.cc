a3a186f72c5c714bdd5670fd8944aa49cb945177
David Shepherd <davidshepherd7@gmail.com>
2012-06-20 10:41:38 +0100

    Fixed SumOfMatrices class.
    Changed sum of matrix tests to use a sparse main matrix with a dense sub-block
    instead of the other way around. This is much closer to my real use for the
    class.
    
    Uncovered a bug in sum of matrices - the map from the added matrix
    multiplication result back to the full matrix was not done properly.
    
    Fixed error checking to allow maps from main to added matrix with size less than
    the number of rows/cols of the added matrix (useful for sparse matrices).

--- sum_matrix_operation_tests/sum_matrix_operation_tests.cc
+++ sum_matrix_operation_tests/sum_matrix_operation_tests.cc
@@ -28,12 +28,13 @@ int main()
 {
   const unsigned mat_size = 500;
   const unsigned n_sparse_values = mat_size;
+  const unsigned dense_block_size = mat_size/10;
   const unsigned max_val = 10000;
   const unsigned n_tests = 10;
 
   Vector<double> norm_error(0);
 
-  for(unsigned i_rand=1; i_rand<n_tests; i_rand++)
+  for(unsigned i_rand=0; i_rand<n_tests; i_rand++)
     {
       srand(i_rand);
 
@@ -47,47 +48,72 @@ int main()
       // Create empty sum of matrices matrix
       SumOfMatrices sum_matrix;
 
-      // A random dense matrix
-      DenseDoubleMatrix main_matrix(mat_size,mat_size,0.0);
-      for(unsigned i=0; i<mat_size; i++)
-	for(unsigned j=0; j<mat_size; j++)
-	  main_matrix(i,j) = rand() % max_val;
-
-      // Add some diagonal terms to make it well conditioned
-      for(unsigned i=0; i<mat_size; i++)
-	main_matrix(i,i) += 2*max_val;
-
-      sum_matrix.main_matrix_pt() = &main_matrix;
-
-
-      // A random compressed row form sparse matrix ??ds note that this will probably
-      // give overlapping values, slightly dodgy but works for now.
-      Vector<int> cr_row_index(n_sparse_values), cr_col_index(n_sparse_values);
-      Vector<double> cr_values(n_sparse_values);
+      // A random compressed row form sparse matrix
+      // to avoid overlaps we have exactly one element per row
+      Vector<int> main_row_index(n_sparse_values), main_col_index(n_sparse_values);
+      Vector<double> main_values(n_sparse_values);
 
       for(unsigned i=0; i<n_sparse_values; i++)
 	{
-	  cr_row_index[i] = (rand() % mat_size);
-	  cr_col_index[i] = (rand() % mat_size);
-	  cr_values[i] = (rand() % max_val);
+	  main_row_index[i] = i;
+	  main_col_index[i] = (rand() % mat_size);
+	  main_values[i] = (rand() % max_val);
 	}
 
-      std::sort(cr_row_index.begin(),cr_row_index.end());
+      std::sort(main_row_index.begin(),main_row_index.end());
 
-      // std::cout << cr_row_index << std::endl;
-      // std::cout << cr_col_index << std::endl;
-      // std::cout << cr_values << std::endl;
+      // std::cout << main_row_index << std::endl;
+      // std::cout << main_col_index << std::endl;
+      // std::cout << main_values << std::endl;
 
-      CRDoubleMatrix a_cr_matrix(cr_values,cr_row_index,cr_col_index,mat_size,mat_size);
+      CRDoubleMatrix main_matrix(main_values,main_row_index,main_col_index,mat_size,mat_size);
+      sum_matrix.main_matrix_pt() = &main_matrix;
+
+      // Add a diagonal sparse matrix
+      Vector<int> id_row_index(mat_size), id_col_index(mat_size);
+      Vector<double> id_values(mat_size,2*max_val);
+
+      for(unsigned i=0; i<mat_size; i++)
+	{
+	  id_row_index[i] = i;
+	  id_col_index[i] = i;
+	}
+
+      CRDoubleMatrix id_matrix(id_values,id_row_index,id_col_index,mat_size,mat_size);
       std::map<long unsigned, long unsigned> row_map, col_map;
       identity_map(row_map, mat_size); identity_map(col_map, mat_size);
+      sum_matrix.add_matrix(&id_matrix,&row_map,&col_map,0);
 
-      // std::cout << col_map << std::endl;
+      // A random dense matrix
+      DenseDoubleMatrix dense_block(dense_block_size,dense_block_size,0.0);
+      for(unsigned i=0; i<dense_block_size; i++)
+      	for(unsigned j=0; j<dense_block_size; j++)
+      	  dense_block(i,j) = rand() % max_val;
 
+      std::map<long unsigned, long unsigned> dense_row_map, dense_col_map;
+      for(unsigned j=0; j<dense_block_size; j++)
+	{
+	  // Get random row and col which are not already used
+	  unsigned new_rand_row, new_rand_col;
+	  do
+	    {
+	      new_rand_row = rand() % mat_size;
+	    }
+	  while(dense_row_map.find(new_rand_row) != dense_row_map.end());
 
-      sum_matrix.add_matrix(&a_cr_matrix, &row_map, &col_map);
-      // a_cr_matrix.sparse_indexed_output(std::cout);
+	  do
+	    {
+	      new_rand_col = rand() % mat_size;
+	    }
+	  while(dense_col_map.find(new_rand_col) != dense_col_map.end());
 
+	  std::pair<long unsigned, long unsigned>
+	    row_map_pair(new_rand_row,j), col_map_pair(new_rand_col,j);
+	  dense_row_map.insert(row_map_pair);
+	  dense_col_map.insert(col_map_pair);
+	}
+
+      sum_matrix.add_matrix(&dense_block, &dense_row_map, &dense_col_map);
 
       // A random rhs vector
       DoubleVector rhs;
@@ -100,19 +126,24 @@ int main()
       ////////////////////////////////////////////////////////////
       DenseDoubleMatrix total_matrix(mat_size,mat_size,0.0);
       for(unsigned i=0; i<mat_size; i++)
-	for(unsigned j=0; j<mat_size; j++)
-	  total_matrix(i,j) = main_matrix(i,j); // manual copy..
-
-      // Add on sparse matrix values
-      for(unsigned i=0; i<n_sparse_values; i++)
 	{
-	  unsigned row = cr_row_index[i];
-	  unsigned col = cr_col_index[i];
-	  total_matrix(row,col) += cr_values[i];
+      	for(unsigned j=0; j<mat_size; j++)
+	  {
+	    total_matrix(i,j) = main_matrix(i,j) + id_matrix(i,j);
+
+	    std::map<long unsigned, long unsigned>::iterator
+	      row_it = dense_row_map.find(i), col_it = dense_col_map.find(j);
+
+	    if((row_it != dense_row_map.end()) && (col_it != dense_col_map.end()))
+	      {
+		total_matrix(i,j) += dense_block(row_it->second,
+						 col_it->second);
+	      }
+	  }
 	}
 
       // main_matrix.output(std::cout);
-      // a_cr_matrix.sparse_indexed_output(std::cout);
+      // main_matrix.sparse_indexed_output(std::cout);
       // total_matrix.output(std::cout);
 
       DoubleVector direct_solution(dist);
@@ -124,7 +155,7 @@ int main()
       //////////////////////////////////////////////////////
 
       GMRES<CRDoubleMatrix> gmres_solver;
-      gmres_solver.tolerance() = 1e-9;
+      gmres_solver.tolerance() = 1e-8;
       // gmres_solver.Setup_preconditioner_before_solve
       DoubleVector solve_solution(dist,1.0);
       gmres_solver.solve(&sum_matrix, rhs, solve_solution);
