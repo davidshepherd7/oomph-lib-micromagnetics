4574d44fd3f1815cc831895295ae4644f7b9cf30
David Shepherd <davidshepherd7@gmail.com>
2012-10-29 10:11:21 +0000

    Generalised ImplicitLLGExchangeProblem
    Renamed ImplicitLLGExchangeProblem -> ImplicitLLGProblem.
    
    New constructors for ImplicitLLGProblem to allow use in a more general
    problem.
    
    Added Applied_field_fct_pt (to allow non-exchange fields).
    
    Updated llg_exchange_test.cc for these changes.

--- implicit_llg_exchange_problem.h
+++ implicit_llg_exchange_problem.h
@@ -2,10 +2,14 @@
 #define OOMPH_IMPLICIT_LLG_EXCHANGE_PROBLEM_H
 
 #include "generic.h"
-#include "../boundary_element_handler.h"
-#include "../micromagnetics_element.h"
-#include "../micromagnetics_element.cc" //??ds shouldn't need...
-#include "../vector_helpers.h"
+#include "./boundary_element_handler.h"
+#include "./micromagnetics_element.h"
+#include "./micromagnetics_element.cc" //??ds shouldn't need...
+#include "./vector_helpers.h"
+
+// Default mesh
+#include "meshes/simple_rectangular_tri_mesh.h"
+
 
 namespace oomph
 {
@@ -14,7 +18,7 @@ namespace oomph
   ///
   // ============================================================
   template<class ELEMENT>
-  class ImplicitLLGExchangeProblem : public Problem
+  class ImplicitLLGProblem : public Problem
   {
   public:
 
@@ -23,40 +27,72 @@ namespace oomph
                                   Vector<double> &m);
 
     // Function pointer for applied field.
-    typedef typename ELEMENT::TimeSpaceToDoubleVectFctPt AppliedFieldFctPt; 
-    
-    /// Default constructor
-    ImplicitLLGExchangeProblem(const unsigned &nx,
-                               const unsigned &ny,
-                               const double &lx,
-                               const double &ly,
-                               AppliedFieldFctPt applied_field_pt)
-      
+    typedef typename ELEMENT::TimeSpaceToDoubleVectFctPt AppliedFieldFctPt;
+
+    /// Default constructor - do nothing except nulling pointers.
+    ImplicitLLGProblem() :
+      Ele_pt(0), Applied_field_fct_pt(0) {}
+
+    /// Make a problem with the default mesh and timestepper
+    ImplicitLLGProblem(const unsigned &nx,
+                       const unsigned &ny,
+                       const double &lx,
+                       const double &ly,
+                       AppliedFieldFctPt input_applied_field_pt) :
+      Ele_pt(0), Applied_field_fct_pt(input_applied_field_pt)
     {
-      
       // Create timestepper
-      BDF<2>* ts_pt = new BDF<2>;
-      add_time_stepper_pt(ts_pt); 
+      BDF<2>* bdf2_pt = new BDF<2>;
+      add_time_stepper_pt(bdf2_pt);
 
       // Create mesh
-      mesh_pt() = new SimpleRectangularTriMesh<ELEMENT>(nx,ny,lx,ly,ts_pt); 
+      mesh_pt() = new SimpleRectangularTriMesh<ELEMENT>(nx,ny,lx,ly,bdf2_pt);
 
-      // Set magnetic parameters: all fields zero except exchange which has
-      // coefficient 1, |M| = 1.
-      ExchangeOnlyParameters.set_simple_llg_parameters();
+      // Set up some simple parameters
+      mag_parameters_pt()->set_simple_llg_parameters();
 
-      // Fiddle with exchange strength if needed.
-      ExchangeOnlyParameters.exchange_constant() /= 5;
-      //??ds complicated because of normalisation. To get |Hex| = 1 we have
-      //to get the exchange constant to mu0. Here we want |Hex| = 0.2 so
-      //divide by 5...
-      ExchangeOnlyParameters.gilbert_damping() = 0.1;
-      
-      ExchangeOnlyParameters.output(std::cout);
+      // Build the problem
+      build();
+    }
+
+    // /// Alternative constructor with any mesh and timestepper.
+    // // (we have to ask for timestepper_pt because it must have already been
+    // // set in the mesh but it also needs to be set in the problem).
+    // ImplicitLLGProblem(Mesh* input_mesh_pt, TimeStepper* time_stepper_pt,
+    //                    AppliedFieldFctPt input_applied_field_pt) : //??ds should be able to passi
+    //   Ele_pt(0), Applied_field_fct_pt(input_applied_field_pt)
+    // {
+    //   // Set mesh, timestepper and applied field
+    //   mesh_pt() = input_mesh_pt;
+    //   add_time_stepper_pt(time_stepper_pt);
+
+    //   // Everything else
+    //   build();
+    // }
+
+    /// Function that does the real work of the constructors.
+    void build()
+    {
+#ifdef PARANOID
+      if((mesh_pt() == 0) || (applied_field_fct_pt() == 0)
+         || (time_stepper_pt() == 0))
+        {
+          std::ostringstream error_msg;
+          error_msg << "Must assign mesh, timestepper and applied "
+                    << "field pointers must be set "
+                    << "before calling build().";
+          throw OomphLibError(error_msg.str(),
+                              "ImplicitLLGProblem::build()",
+                              OOMPH_EXCEPTION_LOCATION);
+        }
+#endif
+
+      // Write out parameters data.
+      Magnetic_parameters.output(std::cout);
 
       // Fiddle with newton paramters if needed
-      // max_residuals() *= 2; 
-      
+      // max_residuals() *= 2;
+
       // Get a pointer to an element for use later
       Ele_pt = dynamic_cast<ELEMENT*>(mesh_pt()->element_pt(0));
 
@@ -71,8 +107,8 @@ namespace oomph
       //         Node* nd_pt = mesh_pt()->boundary_node_pt(b,nd);
       //         for(unsigned j=0; j<Dim; j++)
       //           {
-      //             nd_pt->pin(m_index(j)); 
-      //             nd_pt->set_value(m_index(j),0.0); 
+      //             nd_pt->pin(m_index(j));
+      //             nd_pt->set_value(m_index(j),0.0);
       //           }
       //       }
       //   }
@@ -86,29 +122,29 @@ namespace oomph
           elem_pt->time_pt() = time_pt();
 
           // Set values for magnetic parameters
-          elem_pt->magnetic_parameters_pt() = &ExchangeOnlyParameters;
+          elem_pt->magnetic_parameters_pt() = mag_parameters_pt();
 
           // Set pointer for an applied field
-          elem_pt->applied_field_pt() = applied_field_pt; 
+          elem_pt->applied_field_pt() = applied_field_fct_pt();
         }
 
       // Pin all phi dofs...??ds remove them from element?
       for(unsigned nd=0, nnode=mesh_pt()->nnode(); nd<nnode; nd++)
         {
           Node* nd_pt = mesh_pt()->node_pt(nd);
-          
+
           nd_pt->pin(phi_index());
           nd_pt->pin(phi_1_index());
           nd_pt->set_value(phi_index(),0.0);
-          nd_pt->set_value(phi_1_index(),0.0); 
+          nd_pt->set_value(phi_1_index(),0.0);
         }
-      
+
       // Do equation numbering
-      std::cout <<"Number of equations: " << assign_eqn_numbers() << std::endl; 
+      std::cout <<"Number of equations: " << assign_eqn_numbers() << std::endl;
     }
-    
+
     /// Destructor
-    ~ImplicitLLGExchangeProblem()
+    ~ImplicitLLGProblem()
     {
       // mesh is cleaned up by problem base class
       // timestepper is cleaned up by problem base class
@@ -126,73 +162,93 @@ namespace oomph
           for(unsigned j=0; j<3; j++) nd_pt->set_value(m_index(j),m_values[j]);
         }
     }
-    
+
 
     void actions_after_newton_solve()
     {
       // If we're using BDF we need to keep M normalised.
       if(dynamic_cast<BDF<2>* >(time_stepper_pt()) != 0)
         {
-          renormalise_magnetisation(); 
-        } 
+          renormalise_magnetisation();
+        }
     }
-    
+
 
     /// Output solution
     void doc_solution(DocInfo &doc_info) const
     {
       std::ofstream some_file;
-      char filename[100];
+
 
       // Number of plot points
       unsigned npts = 1;
 
       // Output solution with specified number of plot points per element
-      sprintf(filename,"%s/soln%i.dat", doc_info.directory().c_str(),
-              doc_info.number());
-      some_file.open(filename);
+      std::string filename(doc_info.directory() + "/soln" +
+                           doc_info.number_as_string() + ".dat");
+      some_file.open(filename.c_str());
       mesh_pt()->output(some_file,npts);
       some_file.close();
-      
+
       // Increment number used as output label
-      doc_info.number()++; 
+      doc_info.number()++;
     }
-    
+
     /// Set up an initial M
     void set_initial_condition(const InitialMFctPt initial_m_pt);
-    
-    
+
+
     // Access functions
     // ============================================================
-    
+
     unsigned m_index(const unsigned &j) const
-    {return Ele_pt->m_index_micromag(j);} 
+    {return Ele_pt->m_index_micromag(j);}
 
     unsigned phi_index() const
-    {return Ele_pt->phi_index_micromag();} 
+    {return Ele_pt->phi_index_micromag();}
 
     unsigned phi_1_index() const
-    {return Ele_pt->phi_1_index_micromag();} 
-    
-    
+    {return Ele_pt->phi_1_index_micromag();}
+
+    /// \short Get problem dimension (nodal dimension).
+    const unsigned dim() const
+    {return Dim;}
+
+    /// \short Non-const access function for Applied_field_fct_pt.
+    AppliedFieldFctPt& applied_field_fct_pt() {return Applied_field_fct_pt;}
+
+    /// \short Const access function for Applied_field_fct_pt.
+    AppliedFieldFctPt applied_field_fct_pt() const {return Applied_field_fct_pt;}
+
+    /// \short Non-const access function for Magnetic_parameters_pt.
+    MagneticParameters* mag_parameters_pt()
+    {return &Magnetic_parameters;}
+
+    /// \short Const access function for Magnetic_parameters_pt.
+    const MagneticParameters* mag_parameters_pt() const
+    {return &Magnetic_parameters;}
+
   private:
-    
-    /// Magnetic parameters with all but exchange field zero.
-    MagneticParameters ExchangeOnlyParameters; 
 
     /// A pointer to an element (used to get index numbers)
     const ELEMENT* Ele_pt;
-    
+
     /// The problem dimension
     unsigned Dim;
-        
+
+    /// Pointer to the applied field.
+    AppliedFieldFctPt Applied_field_fct_pt;
+
+    /// Magnetic parameters storage. ??ds should maybe go in meshes?
+    MagneticParameters Magnetic_parameters;
+
     /// Inaccessible copy constructor
-    ImplicitLLGExchangeProblem(const ImplicitLLGExchangeProblem & dummy)
-    {BrokenCopy::broken_copy("ImplicitLLGExchangeProblem");}
-    
+    ImplicitLLGProblem(const ImplicitLLGProblem & dummy)
+    {BrokenCopy::broken_copy("ImplicitLLGProblem");}
+
     /// Inaccessible assignment operator
-    void operator=(const ImplicitLLGExchangeProblem &dummy)
-    {BrokenCopy::broken_assign("ImplicitLLGExchangeProblem");}
+    void operator=(const ImplicitLLGProblem &dummy)
+    {BrokenCopy::broken_assign("ImplicitLLGProblem");}
 
   };
 
@@ -200,7 +256,7 @@ namespace oomph
   /// Set up the initial conditions
   //======================================================================
   template<class ELEMENT>
-  void ImplicitLLGExchangeProblem<ELEMENT>::
+  void ImplicitLLGProblem<ELEMENT>::
   set_initial_condition(const InitialMFctPt initial_m_pt)
   {
     // Backup time in global Time object
@@ -239,7 +295,7 @@ namespace oomph
         // Loop over the nodes to set initial values everywhere
         for (unsigned n=0;n<num_nod;n++)
           {
-            unsigned dim = mesh_pt()->node_pt(n)->ndim(); 
+            unsigned dim = mesh_pt()->node_pt(n)->ndim();
 
             // Get initial value of m from inputs
             Vector<double> m(3,0.0), x(dim,0.0);
@@ -256,7 +312,7 @@ namespace oomph
     this->time_pt()->time()=backed_up_time;
   }
 
-    
+
 }
 
 #endif
--- test_collection/llg_exchange/llg_exchange_test.cc
+++ test_collection/llg_exchange/llg_exchange_test.cc
@@ -14,19 +14,19 @@
   very rough initial m. All m vectors should align to the small applied
   field.
 
- */
+*/
 
 using namespace oomph;
-using namespace MathematicalConstants; 
+using namespace MathematicalConstants;
 
 namespace Inputs
 {
-  double dt = 0.01;
-  double tmax = 50;
+  double dt = 0.1;
+  double tmax = 0.1;
 
-  unsigned nx = 20, ny = nx;
-  double lx = 1.0, ly = lx; 
- 
+  unsigned nx = 10, ny = nx;
+  double lx = 1.0, ly = lx;
+
   void initial_m(const double& t, const Vector<double> &x, Vector<double> &m)
   {
     m.assign(3,0.0);
@@ -39,17 +39,17 @@ namespace Inputs
     m[1] = (1 - m[0]);
     m[2] = 0.0;
 
-    VectorOps::normalise(m); 
+    VectorOps::normalise(m);
   }
 
   //??ds applied field?
-  void applied_field(const double& t, const Vector<double> &x, 
+  void applied_field(const double& t, const Vector<double> &x,
                      Vector<double> &h_app)
   {
     h_app.assign(3,0.0);
-    h_app[0] = 0.01; 
+    h_app[0] = 0.01;
   }
-  
+
 }
 
 
@@ -58,42 +58,36 @@ int main()
   // Enable some floating point error checkers
   feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW);
 
-  double dt = Inputs::dt; 
+  double dt = Inputs::dt;
   double tmax = Inputs::tmax;
-  
+
   // Build problem
-  ImplicitLLGExchangeProblem<TMicromagElement<2,2> > 
+  ImplicitLLGProblem<TMicromagElement<2,2> >
     problem(Inputs::nx, Inputs::ny, Inputs::lx, Inputs::ly,
             Inputs::applied_field);
 
 
+  // Fiddle with exchange strength.
+  problem.mag_parameters_pt()->exchange_constant() = mag_parameters::mu0/5;
+  // Complicated because of normalisation. To get |Hex| = 1 we have to get
+  // the exchange constant to mu0. Here we want |Hex| = 0.2 so divide by
+  // 5...
+  problem.mag_parameters_pt()->gilbert_damping() = 0.1;
 
-  // // Set up linear solver: GMRES
-  // problem.linear_solver_pt() = new GMRES<CRDoubleMatrix>;
-  // IterativeLinearSolver* it_solver_pt =
-  //   dynamic_cast<IterativeLinearSolver*>(problem.linear_solver_pt());
-
-  // /// AMG preconditioner
-  // HyprePreconditioner *amg_pt;
-  // amg_pt = new HyprePreconditioner;
-  // it_solver_pt->preconditioner_pt() = amg_pt;
-  // amg_pt->hypre_method() = HyprePreconditioner::BoomerAMG;
-
-
-
+  //??ds better solver in the end?
 
   // Initialise
-  problem.initialise_dt(dt); 
+  problem.initialise_dt(dt);
   problem.set_initial_condition(Inputs::initial_m);
 
   // Set up output
-  DocInfo doc_info; 
+  DocInfo doc_info;
   doc_info.set_directory("results");
-  
+
   problem.doc_solution(doc_info);
-  
+
   // Timestep
-  unsigned nstep = int(tmax/dt); 
+  unsigned nstep = int(tmax/dt);
   for(unsigned t=0; t < nstep; t++)
     {
       std::cout << "Timestep " << t << std::endl;
@@ -109,6 +103,8 @@ int main()
   // // ??ds
   // // Check solution
   // double avg_m = problem.average_magnetisation();
-  
-  // error checks...
+
+  // error checks not done yet so pretend to fail
+
+  return 244;
 }
