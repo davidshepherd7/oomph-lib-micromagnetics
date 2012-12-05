a450c298f6d5f7567ba17ae0ddcededdd14d140a
David Shepherd <davidshepherd7@gmail.com>
2012-12-05 17:26:42 +0000

    Add an experiment for a single circle

---
+++ experiments/circle_bem/go.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+make -k
+
+as="3 4 5" # 0.001 0.0001"
+
+# rebuild mesh files
+fig2poly circle.fig
+triangle -q20 circle.fig.poly
+triangle -rp -q30 -a1.6 circle.fig.1.poly
+triangle -rp -q30 -a0.4 circle.fig.2.poly
+triangle -rp -q30 -a0.1 circle.fig.3.poly
+triangle -rp -q30 -a0.025 circle.fig.4.poly
+triangle -rp -q30 -a0.00625 circle.fig.5.poly
+
+
+# rebuild corner file
+./poly2corner.sh circle.fig.poly
+
+for a in $as; do
+
+    ./circle_bem -mesh "circle.fig.${a}" -corners "circle.fig.poly.corner"
+    
+    oomph-convert results/*.dat
+
+    mkdir -p results_${a}
+    mv results/* results_${a}
+    
+done
+
+nohup paraview --data="results/field0.vtu" &
