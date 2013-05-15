7d8a90a9c2fa8400c9f72d9ec2adac3233dd6306
David Shepherd <davidshepherd7@gmail.com>
2013-05-15 21:32:30 +0100

    Add a script to run with settings for comparison with nmag's cubeoid example

---
+++ control_scripts/nmag-cubeoid.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+cd semi_implicit_mm_driver
+
+make -k -s
+
+# Make sure directory exists and is empty
+outdir="../../experiments/nmag_cubeoid1"
+touch $outdir
+mv $outdir ${outdir}.bak
+mkdir -p $outdir
+
+# The run command
+./semi_implicit_mm_driver -mesh sq_cubeoid -ref 4 -tol 1e-4 -dt 1e-4 -tmax 40.0 -initm xz -happ zero -solver gmres-amg -mag-params nmag-cubeoid -outdir $outdir
