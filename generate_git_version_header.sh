bb12e22685ac7682feafbe1802bbe8199bdcd741
David Shepherd <davidshepherd7@gmail.com>
2013-05-16 14:06:12 +0100

    Use char* not char[]!

--- generate_git_version_header.sh
+++ generate_git_version_header.sh
@@ -11,8 +11,8 @@ echo "#define OOMPH__GIT_VERSION_H" >> git_version.h
 echo "" >> git_version.h
 echo "namespace GitVersion" >> git_version.h
 echo "{" >> git_version.h
-echo "extern const char[] VERSION = \"$(git rev-parse HEAD)\";" >> git_version.h
-echo "extern const char[] BUILD_TIME = \"$(date)\";" >> git_version.h
+echo "extern const char* VERSION = \"$(git rev-parse HEAD)\";" >> git_version.h
+echo "extern const char* BUILD_TIME = \"$(date)\";" >> git_version.h
 echo "}" >> git_version.h
 echo "" >> git_version.h
 echo "#endif" >> git_version.h
