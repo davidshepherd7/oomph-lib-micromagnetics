b397d6ec5e3f59bcf9516dcb90210deac45475b7
David Shepherd <davidshepherd7@gmail.com>
2013-05-16 17:01:28 +0100

    Small improvements to nmag-cubeoid.sh

--- control_scripts/nmag-cubeoid.sh
+++ control_scripts/nmag-cubeoid.sh
@@ -7,11 +7,14 @@ cd semi_implicit_mm_driver
 
 make -k -s
 
+randstring=$(date | md5sum | tr -d ' '| tr -d '-')
+
+
 # Make sure directory exists and is empty
-outdir="../../experiments/nmag_cubeoid1"
+outdir="../../experiments/nmag_cubeoid_${randstring}"
 touch $outdir
 mv $outdir ${outdir}.bak
 mkdir -p $outdir
 
 # The run command
-./semi_implicit_mm_driver -mesh sq_cubeoid -ref 4 -tol 1e-4 -dt 1e-4 -tmax 40.0 -initm xz -happ zero -solver gmres-amg -mag-params nmag-cubeoid -outdir $outdir
+./semi_implicit_mm_driver -mesh sq_cubeoid -ref 4 -tol 1e-4 -dt 1e-4 -tmax 40.0 -initm xz -happ zero -solver gmres-amg -mag-params nmag-cubeoid -outdir $outdir | tee > $outdir/stdout
