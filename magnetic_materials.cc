b87df311bac56692ca386d3b3839359ad2958caf
David Shepherd <davidshepherd7@gmail.com>
2013-07-09 10:34:31 +0100

    Improvements/fixes to magnetic parameters
    * Correct non-dimensionalisation of exchange and anisotropy fields
      See write-up appendix on non-dimensionalisation.
    
    * Change mu_0 to be a parameter rather than a fixed constant
      So that we can set it to 1 in non-dimensional cases.
    
    * Add function to get easy axis along whichever direction
      Faster and all that is needed most of the time (due to taking dot product
      then squaring).
    
    * Simplify factory functions by writing some in terms of others

--- magnetic_materials.cc
+++ magnetic_materials.cc
@@ -1,8 +1,2 @@
 
 #include "magnetic_materials.h"
-
-namespace mag_parameters
-{
-  // Magnetic constant (source: http://physics.nist.gov/)
-  double mu0 = 12.566370614e-7;  // in N/(A^2) (SI)
-}
--- magnetic_materials.h
+++ magnetic_materials.h
@@ -12,16 +12,12 @@ namespace mag_parameters
       //??ds check the anisotropy fns...
       UNIAXIAL_CRYSTALLINE_ANISOTROPY
     };
-
-  // Magnetic constant
-  extern double mu0; // in N/(A^2) (SI)
 }
 
 
 namespace oomph
 {
 
-  using namespace mag_parameters;
   using namespace StringConversion;
 
   // ============================================================
@@ -33,9 +29,11 @@ namespace oomph
 
   public:
     MagneticParameters()
-      : Gamma(2.211e5), Gilbert_damping(0.05),
+      : Gamma(2.211e5),
+        Mu0(12.566370614e-7),  // in N/(A^2) (SI)
+        Gilbert_damping(0.05),
         Saturation_magnetisation(1.0),
-        Exchange_constant(0.5 * mag_parameters::mu0), // gives exchange strength = 1
+        Exchange_constant(0.5 * mu0()), // gives exchange strength = 1
         K1(0.0),
         Easy_axis(3,0),
         Distance_units(1e-9),
@@ -64,13 +62,14 @@ namespace oomph
     double ca_debug_coeff() const {return Ca_debug_coeff;}
     double distance_units() const {return Distance_units;}
     bool surface_anisotropy_enabled() const {return Surface_anisotropy_enabled;}
+    double mu0() const {return Mu0;}
 
     // Coefficients of each of the fields (before normalisation).
     double hk() const
-    {return (2 * k1()) / (mag_parameters::mu0 * saturation_magnetisation()); }
+    {return (2 * k1()) / (mu0() * saturation_magnetisation()); }
     double hex() const
     {return (2 * exchange_constant()) /
-        (mag_parameters::mu0 * saturation_magnetisation()
+        (mu0() * saturation_magnetisation()
          * distance_units() * distance_units());}
     double hms() const
     {return saturation_magnetisation();}
@@ -85,28 +84,50 @@ namespace oomph
     {return gilbert_damping();}
     double normalised_saturation_magnetisation() const
     {return 1.0;}
-    double normalised_hex() const
-    {return hex() * field_normalisation_factor() * exchange_debug_coeff();}
-    double normalised_hk() const //??ds probably need some distance conversion in here...
-    {return hk() * field_normalisation_factor() * ca_debug_coeff();}
-    double normalised_hms() const
-    {return hms() * field_normalisation_factor() * magnetostatic_debug_coeff();}
 
-    // Normalise by magnetostatic field strength
+    /// \short We are normalising field by Ms: this function allows it to
+    /// be used elsewhere (e.g. for applied field).
     double field_normalisation_factor() const
     {
-      // return 1/hex_str;
-      return 1/hms();
+      // Note that all normalised fields will need to be changed (i.e. the
+      // normalised_x() functions below) if you want to normalise by
+      // something else
+      return 1/saturation_magnetisation();
     }
+
+    double normalised_hex() const
+    {
+      return (2 * exchange_constant() / (mu0() *
+                                     std::pow(saturation_magnetisation(), 2) *
+                                     std::pow(distance_units(), 2)))
+        * exchange_debug_coeff();
+    }
+
+    double normalised_hk() const
+    {
+      return (2 * k1() / (mu0() *
+                      std::pow(saturation_magnetisation(), 2)))
+        * ca_debug_coeff();
+    }
+
+    double normalised_hms() const
+    {return 1.0* magnetostatic_debug_coeff();}
 
     // ??ds need to make sure all field normalisation factors are the same for all
     // meshes or this falls apart!
     double time_normalisation_factor() const
-    {return (1/gamma()) * field_normalisation_factor();}
+    {return (1/gamma() * hms());}
 
+
+    /// \short get easy axis direction unit vector in whatever direction it
+    /// was given in.
+    Vector<double> easy_axis() const
+      {
+        return Easy_axis;
+      }
 
     /// \short get easy axis direction unit vector closest to m.
-    Vector<double> easy_axis(const Vector<double> &m) const
+    Vector<double> nearest_easy_axis(const Vector<double> &m) const
       {
         Vector<double> easy_axis(Easy_axis.size(), 0);
 
@@ -119,10 +140,7 @@ namespace oomph
           }
         else
           {
-            for(unsigned j=0; j<Easy_axis.size(); j++)
-              {
-                easy_axis[j] = Easy_axis[j];
-              }
+            easy_axis = Easy_axis;
           }
 
         return easy_axis;
@@ -138,15 +156,16 @@ namespace oomph
       if(crystalline_ansiotropy_type() ==
          mag_parameters::UNIAXIAL_CRYSTALLINE_ANISOTROPY)
         {
-          Vector<double> e = easy_axis(m);
+          Vector<double> e = easy_axis();
 
-          // h_ca is easy direction multiplied by the magnitude
-          double magnitude = std::abs(VectorOps::dot(e, m));
+          // h_ca is easy direction multiplied by the magnitude (which is
+          // negative if closer to -e than e).
+          double magnitude = normalised_hk() * VectorOps::dot(e, m);
 
           h_ca.assign(e.size(), 0.0);
           for(unsigned i=0; i<e.size(); i++)
             {
-              h_ca[i] = e[i] * magnitude * normalised_hk();
+              h_ca[i] = e[i] * magnitude;
             }
         }
       else
@@ -164,7 +183,7 @@ namespace oomph
     double exchange_length() const
     {
       double l1 = std::sqrt( (2* exchange_constant() )
-                             / (mag_parameters::mu0 * saturation_magnetisation()
+                             / (mu0() * saturation_magnetisation()
                                 * saturation_magnetisation()));
       double l2;
       if (k1() > 0)
@@ -188,19 +207,18 @@ namespace oomph
 
         case mag_parameters::UNIAXIAL_CRYSTALLINE_ANISOTROPY:
 
-          Vector<double> easy_axis(3,0.0);
-          unsigned ax = 0;
+          // Get easy axis
+          Vector<double> e = easy_axis();
 
-          // Find which direction along the axis we want
-          if(m[ax] < 0)
-            easy_axis[ax] = -1.0;
-          else
-            easy_axis[ax] = +1.0;
-
+          // Calculate derivatives
           for(unsigned j=0; j<3; j++)
-            for(unsigned i=0; i<3; i++)
-              dhcadm[i][j] = shape_fn_l2_at_x
-                * easy_axis[i] * easy_axis[j] * normalised_hk() ;
+            {
+              for(unsigned i=0; i<3; i++)
+                {
+                  dhcadm[i][j] = shape_fn_l2_at_x
+                    * e[i] * e[j] * normalised_hk();
+                }
+            }
           break;
         }
     }
@@ -210,10 +228,13 @@ namespace oomph
       stream << std::endl;
       stream << "Magnetic parameters are:" << std::endl;
       stream << "Gamma = " << gamma() << std::endl;
+      stream << "Mu0 = " << mu0() << std::endl;
       stream << "Damping = " << gilbert_damping() << std::endl;
       stream << "M_s = "  << saturation_magnetisation() << std::endl;
       stream << "Exchange constant = " << exchange_constant() <<std::endl;
       stream << "K_1 = " << k1() << std::endl;
+      stream << "Easy_axis = " << easy_axis() << std::endl;
+
       stream << "magnetostatic debug = " << magnetostatic_debug_coeff() << std::endl;
       stream << "exchange debug = " << exchange_debug_coeff() << std::endl;
       stream << "boundary exchange debug = " << boundary_exchange_debug_coeff() << std::endl;
@@ -248,6 +269,7 @@ namespace oomph
     double& boundary_exchange_debug_coeff() {return Boundary_exchange_debug_coeff;}
     double& ca_debug_coeff() {return Ca_debug_coeff;}
     double& distance_units() {return Distance_units;}
+    double& mu0() {return Mu0;}
 
     void set_cubic_anisotropy()
     {
@@ -257,6 +279,7 @@ namespace oomph
 
   private:
     double Gamma;
+    double Mu0;
     double Gilbert_damping;
 
     double Saturation_magnetisation;
@@ -310,50 +333,43 @@ namespace oomph
           parameters_pt->gamma() = 2.210173e5; // m/(As)
         }
 
-      // Set parameters to remove all coefficients except gilbert damping
-      // from the llg. Set gilbert damping to 0.5.
+      // Set parameters as many coefficients as possible from the LLG. Set
+      // damping = 0.5, hex = 1, hk = 0.
       else if(to_lower(parameters_name) == "simple-llg")
         {
           parameters_pt = new MagneticParameters;
           parameters_pt->saturation_magnetisation() = 1.0; // normalised units
-          parameters_pt->exchange_constant() = 0.5 * mag_parameters::mu0; // this gives hex = 1
+          parameters_pt->exchange_constant() = 0.5; // this gives hex = 1
           parameters_pt->k1() = 0.0;
           parameters_pt->distance_units() = 1;
-
+          parameters_pt->mu0() = 1;
           parameters_pt->gamma() = 1;
           parameters_pt->gilbert_damping() = 0.5;
         }
 
-      // Set parameters to remove all coefficients except gilbert damping
-      // and K1 from the llg. Set gilbert damping to 0.5, set K1, e_hat
-      // s.t. anisotropy field = 1 and is along z.
+      // Set parameters as many coefficients as possible from the LLG. Set
+      // damping = 0.5, hex = 1, hk = 1.
       else if(to_lower(parameters_name) == "simple-llg-anisotropy")
         {
-          parameters_pt = new MagneticParameters;
-          parameters_pt->saturation_magnetisation() = 1.0; // normalised units
-          parameters_pt->exchange_constant() = 0.5 * mag_parameters::mu0; // this gives hex = 1
-          parameters_pt->k1() = 0.5 * mag_parameters::mu0; //gives hk = 1
-          parameters_pt->distance_units() = 1;
-          // z easy axis is default
-
-          parameters_pt->gamma() = 1;
-          parameters_pt->gilbert_damping() = 0.5;
+          parameters_pt = magnetic_parameters_factory("simple-llg");
+          parameters_pt->k1() = 0.5; // This gives hk = 1
         }
 
-      // Set parameters to remove all coefficients except gilbert damping
-      // from the llg. Set gilbert damping to 0.5.
+      // Set parameters as many coefficients as possible from the LLG. Set
+      // damping = 1, hex = 1, hk = 0.
       else if(to_lower(parameters_name) == "simple-llg-max-damped")
         {
-          parameters_pt = new MagneticParameters;
-          parameters_pt->saturation_magnetisation() = 1.0; // normalised units
-          parameters_pt->exchange_constant() = 0.5 * mag_parameters::mu0; // this gives hex = 1
-          parameters_pt->k1() = 0.0;
-          parameters_pt->distance_units() = 1;
-
-          parameters_pt->gamma() = 1;
-          parameters_pt->gilbert_damping() = 1.0;
+          parameters_pt = magnetic_parameters_factory("simple-llg");
+          parameters_pt->gilbert_damping() = 1;
         }
 
+      else
+        {
+          std::string error_msg = "Unrecognised parameters_name: "
+            + to_lower(parameters_name);
+          throw OomphLibError(error_msg, OOMPH_CURRENT_FUNCTION,
+                              OOMPH_EXCEPTION_LOCATION);
+        }
 
       return parameters_pt;
     }
