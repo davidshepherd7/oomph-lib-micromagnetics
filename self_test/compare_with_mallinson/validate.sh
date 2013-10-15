e54976587eb5922eec096764e1e58848974b73ea
David Shepherd <davidshepherd7@gmail.com>
2013-10-15 13:30:53 +0100

    Add test to compare with Mallinson switching times

---
+++ self_test/compare_with_mallinson/validate.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+set -o errexit
+
+HAVE_PYTHON=$1
+
+set -o nounset
+
+new_clean_dir()
+{
+    touch $1
+    rm -r $1
+    mkdir $1
+}
+
+# An fpdiff wrapper that always puts the output in the right place and
+# checks if we can run fpdiff.
+VALDIR=$(pwd)/Validation
+wrapped_fpdiff()
+{
+    mkdir -p $VALDIR
+
+    if test "$HAVE_PYTHON" = "no_fpdiff"; then
+        echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >> validation.log
+    else
+        ../../../../bin/fpdiff.py $@ >> $VALDIR/validation.log
+    fi
+}
+
+
+TPWD=$(pwd)
+CONTROL_SCRIPTS="../../control_scripts"
+MM_LIB_DIR="../../"
+
+# Make sure it's all ready
+make -k -s -C $MM_LIB_DIR
+make -k -s -C $MM_LIB_DIR install
+new_clean_dir Validation
+
+
+# llg residual
+# ============================================================
+
+# Run simulation with llg residual. Midpoint is ESSENTIAL to get low enough
+# errors without tiny step size
+llg_dir="$TPWD/Validation/llg"
+new_clean_dir $llg_dir
+cd $CONTROL_SCRIPTS/llg_driver/
+./llg_driver -resi llg -tmax 0.3 -dt 1e-2 -solver superlu -happ minus_z \
+    -ts midpoint -newton-tol 1e-12 \
+    -initm z -outdir $llg_dir -mag-params 'simple-llg' \
+    > $llg_dir/stdout
+
+# Check the errors are small by comparing with a file full of zeros
+cd $TPWD
+cut -d\; -f 4 < $llg_dir/trace > $llg_dir/time_error_norms
+wrapped_fpdiff $llg_dir/time_error_norms validata/zeros 0 1e-5
+
+
+# same for ll residual
+# ============================================================
+
+
+# Run simulation with ll residual
+ll_dir="$TPWD/Validation/ll"
+new_clean_dir $ll_dir
+cd $CONTROL_SCRIPTS/llg_driver/
+./llg_driver -resi ll -dt 1e-2 -tmax 0.3 -solver superlu -happ minus_z \
+    -ts midpoint \
+    -initm z -outdir $ll_dir -mag-params 'simple-llg' \
+    > $ll_dir/stdout
+
+# Check the errors are small
+cd $TPWD
+cut -d\; -f 4 < $ll_dir/trace > $ll_dir/time_error_norms
+wrapped_fpdiff $ll_dir/time_error_norms validata/zeros 0 1e-5
