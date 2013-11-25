f1a61f735d960aa50709f876c15b16c72b0e7c94
David Shepherd <davidshepherd7@gmail.com>
2013-11-25 22:10:40 +0000

    Remove all the old experiments folders

--- experiments/circle_bem/poly2corner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o nounset
-
-# Extract x and y coords for nodes from a .poly file
-
-# remove first line, remove comments, remove all after first blank line (i.e. only care about nodes) then take only fields 2 and 3 from what remains (i.e. x and y coords).
-cat $1 | sed '1d' | sed 's/#.*//g' | sed -n '/^$/q;p' | cut -f2,3 -d' ' | perl -pe 'chomp if eof' > $1.corner
