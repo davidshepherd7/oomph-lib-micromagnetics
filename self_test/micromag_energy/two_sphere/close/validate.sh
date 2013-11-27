9c32b5d2116299a5b5674bcc2d2ffa90a6e032ce
David Shepherd <davidshepherd7@gmail.com>
2013-11-27 15:48:08 +0000

    Add self tests for multiple meshes (two spheres, close or far away)
    * Close not working yet, need to do some calculations...

---
+++ self_test/micromag_energy/two_sphere/close/validate.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+micromagroot="../../../.."
+CONTROL_SCRIPTS="$micromagroot/control_scripts"
+MM_LIB_DIR="$micromagroot"
+
+# source common commands for energy tests
+. "$micromagroot/self_test/micromag_energy/init_validate.sh"
+
+# Two close spheres test
+# ============================================================
+
+# Now test that the energy is ~ doubled when we run for two well separated
+# spheres.
+
+TWO_SPHERE_DIR=$TPWD/Validation
+new_clean_dir $TWO_SPHERE_DIR
+
+# Run simulation
+cd $CONTROL_SCRIPTS/semi_implicit_mm_driver/
+./semi_implicit_mm_driver -dt 0.001 -tmax 0.001 -ref 3 \
+    -solver superlu -happ minus_z -initm exactly_z -outdir $TWO_SPHERE_DIR \
+    -mag-params 'simple-llg' \
+    -mesh multi_ut_sphere -xshift 1.1 \
+    > $TWO_SPHERE_DIR/stdout
+
+# Extract + check energies
+final_energy $TWO_SPHERE_DIR/trace > $TWO_SPHERE_DIR/energies
+wrapped_fpdiff $TWO_SPHERE_DIR/energies $TPWD/../two_sphere_energies 15 1e-4
+
+# ??ds this should fail for now
---
+++ self_test/micromag_energy/two_sphere/well_separated/validate.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+micromagroot="../../../.."
+CONTROL_SCRIPTS="$micromagroot/control_scripts"
+MM_LIB_DIR="$micromagroot"
+
+# source common commands for energy tests
+. "$micromagroot/self_test/micromag_energy/init_validate.sh"
+
+
+# Two separate sphere test
+# ============================================================
+
+# Now test that the energy is ~ doubled when we run for two well separated
+# spheres.
+
+TWO_SPHERE_DIR=$TPWD/Validation
+new_clean_dir $TWO_SPHERE_DIR
+
+# Run simulation
+cd $CONTROL_SCRIPTS/semi_implicit_mm_driver/
+./semi_implicit_mm_driver -dt 0.001 -tmax 0.001 -ref 3 \
+    -solver superlu -happ minus_z -initm exactly_z -outdir $TWO_SPHERE_DIR \
+    -mag-params 'simple-llg' \
+    -mesh multi_ut_sphere -xshift 20 \
+    > $TWO_SPHERE_DIR/stdout
+
+# Extract + check energies
+final_energy $TWO_SPHERE_DIR/trace > $TWO_SPHERE_DIR/energies
+wrapped_fpdiff $TWO_SPHERE_DIR/energies $TPWD/../two_sphere_energies 15 1e-4
+
+
+
+# If M_s=1, r=1, H_app=[0,0,-1.1] (numbers from copying formula into python)
+# E_ms = -0.5 * 1 * (-0.33333 * 1) * (4.0/3) * pi = 0.6981247194807239
+# E_zee = -1 * -1.1 * (4.0/3) * pi = 4.60766922526503
+# E_ca = 0 (because the magnetics is aligned with easy axis)
+
+
+# Two well separated spheres have double this energy
+
+# Unfortunately good approximations to a sphere are EXPENSIVE.... so it's
+# only roughly right :( Hence 1e-4 as numerical zero and 15% error allowed.
