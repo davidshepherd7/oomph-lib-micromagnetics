ce1de82f5810f1767f4897e4fcfbc9e0a37dcc1a
David Shepherd <davidshepherd7@gmail.com>
2014-01-09 18:15:32 +0000

    Move some code out of parse.py into a general python header

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Code to extend oomph-lib to solve micromagnetics problems.
 
 To use you will need a full install of oomph-lib (see https://github.com/davidshepherd7/oomph-lib and http://oomph-lib.maths.man.ac.uk/doc/html/index.html) then:
 
+* Add ./etc/ directory to your python path variable
 * Place this repository in a subdirectory of `user_drivers`
 * Build oomph-lib (using `autogen.sh` in the root directory)
 * Do `make && make install` in root of this repository (this installs the libraries in oomph-libs lib directory)
+* Create the meshes with ./control_scripts/mesh_generation.py
 
-The main binaries of interest are `control_scripts/semi_implicit_mm_driver/semi_implicit_mm_driver` and `control_scripts/llg_driver/llg_driver`. Run make in their directories to build them. Run the binaries with --help to see a list of command line flags.
+The main binary of interest is `control_scripts/driver/driver`. Run with --help to see a list of command line flags.
 
-There are also a number of useful scripts in `control_scripts`. In particular `mesh_construction.py` should be run to create the unstructured meshes (requires `tetgen` and `triangle`).
+There are also a number of useful scripts in `control_scripts`. In particular `mesh_construction.py` can be used to create various unstructured meshes (requires `tetgen` and `triangle`).
--- control_scripts/parse.py
+++ control_scripts/parse.py
@@ -15,6 +15,9 @@ import os
 import os.path
 import ast
 
+import oomphpy
+import oomphpy.micromagnetics as mm
+
 import itertools as it
 import functools as ft
 import scipy as sp
@@ -53,67 +56,6 @@ def parse_info_file(filename):
     return info_dict
 
 
-def parse_trace_file(filename):
-    """Read data from a trace file into a dict of lists keyed on the column
-    header.
-
-    Assuming the file is in the format
-
-        col1_header col2_header ...
-        col1_value1 col2_value1 ...
-        col1_value2 col2_value2 ...
-        .                .
-        .                .
-        .                .
-
-    """
-    # Convert to new deliminator with "find -name 'trace' | xargs sed -e
-    # 's/ /; /g' -i" might not work for very big files due to limitations
-    # in sed -i?
-
-    # Get the file data as a list of strings
-    with open(filename) as f:
-        lines = f.read().splitlines()
-
-    def mysplit(line):
-        # Split on '; ' and skip the last entry (because we have a final ';
-        # ' at the end of lines).
-        return line.split('; ')#[:-1]
-
-    # Split the raw strings into data fields (so we now have a list of strings)
-    headers = mysplit(lines[0])
-    body = [mysplit(l) for l in lines[1:]]
-
-    # Make an empty dict to store our data in
-    data = {}
-
-    # Read in data from body in columns and prepend the header for that
-    # column.
-    for header, col in zip(headers, zip(*body)):
-
-        # Convert the data in "col" from strings to python data types
-        # (e.g. floats, lists). If it's a list (or list of lists or...)
-        # type then convert to an sp.array.
-        real_data = maybe_recursive_convert_to_array([ast.literal_eval(c) for c in col])
-
-        # Put it into our dict with the header of the column as the key and
-        # the rest as the data.
-        data[header] = real_data
-
-
-    # Remove 'dummy' columns if we had any (may have used them for padding)
-    try:
-        del data['dummy']
-    except KeyError:
-        pass
-
-    # Calculate time stamp differences (rough value for wall clock time per
-    # step).
-    data['unix_timestamp_diffs'] = differences(data['unix_timestamp'])
-
-    return data
-
-
 def parse_run(results_folder):
     """Parse the info file, trace file and look for FAILED file in a folder
     and put all data into a dict with keys taken from file data.
@@ -125,7 +67,7 @@ def parse_run(results_folder):
     except IOError:
         return None
 
-    trace_dict = parse_trace_file(pjoin(results_folder, "trace"))
+    trace_dict = mm.parse_trace_file(pjoin(results_folder, "trace"))
 
     # Add the data from trace file into the dict (NOTE: if any fields have
     # the same name then the trace file data will "win").
@@ -212,36 +154,6 @@ def split_up_stuff(big_dict_list, keys_to_split_on=None):
                         if existing_items_match(test_dict, d)])
 
     return newlist
-
-
-def is_iterable(item):
-    # Add types here you don't want to mistake as iterables
-    if isinstance(item, basestring):
-        return False
-
-    # Fake an iteration.
-    try:
-        for x in item:
-            break
-    except TypeError:
-        return False
-
-    return True
-
-def maybe_recursive_convert_to_array(arr):
-    """Convert lists to arrays, lists of lists to arrays of arrays etc. Also
-    works for any iterable. Non-iterable types are left alone.
-    """
-    if is_iterable(arr):
-        return sp.array([maybe_recursive_convert_to_array(a) for a in arr])
-    else:
-        return arr
-
-def differences(arr):
-    """Get an array containing the differences between consecutive values of an
-    array. First value is None.
-    """
-    return [None] + [a - b for a, b in zip(arr[1:], arr[:-1])]
 
 
 # Plotting functions
