384efd2b68c96504498a445b27efda1745115627
David Shepherd <davidshepherd7@gmail.com>
2014-01-14 20:19:25 +0000

    Fix output redirection in self tests using validate.py

--- self_test/explicit_llg/validate.sh
+++ self_test/explicit_llg/validate.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
+mkdir -p Validation
 # Run the real script and exit with it's error status
-./validate.py &> Validation/validation.log
-exit $?
+./validate.py 2>&1 > Validation/validation.log
