2637a6955859c93356021b8c4a38e365120ee6e9
David Shepherd <davidshepherd7@gmail.com>
2014-02-20 12:02:58 +0000

    Add test for accuracy of semi implicit method

---
+++ self_test/semi_implicit_ms_accuracy/validate.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Python 2/3 compatability
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import sys
+import argparse
+import os
+import os.path
+
+import itertools as it
+
+from os.path import join as pjoin
+
+# Make sure *this* versions oomphpy is in the path (before any other
+# versions in other places)
+sys.path.insert(1, pjoin(os.path.dirname(__file__), "../../etc"))
+import oomphpy
+import oomphpy.micromagnetics as mm
+import oomphpy.tests as tests
+
+
+def main():
+
+    # What to run
+    argdicts = {
+        "-driver" : 'llg',
+        "-solver" : "som-gmres",
+        "-prec" : "som-main-exact",
+        '-tmax' : 1,
+        '-dt' : [0.1],
+        # '-newton-tol' : [1e-8, 1.1e-8],
+        '-ts' : ['bdf2', 'midpoint-bdf'],
+        '-ms-method' : ['implicit', 'decoupled'],
+        }
+
+    # Where it's going to end up
+    base_outdir = os.path.abspath(pjoin(os.path.dirname(__file__), "Validation"))
+
+    # Run
+    err_codes, outdirs = mm.run_sweep(argdicts, base_outdir)
+
+    # Get data
+    datasets = list(map(mm.parse_run, outdirs))
+
+    # Check things
+    ran = all((e == 0 for e in err_codes))
+    t1 = tests.check_solns_match_key(datasets, '-ms-method')
+
+    if ran and t1:
+        return 0
+    else:
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
