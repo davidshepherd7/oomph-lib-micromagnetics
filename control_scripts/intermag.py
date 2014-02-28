7bc7b0041751b9661253420420a2aa74efb30f6b
David Shepherd <davidshepherd7@gmail.com>
2014-02-28 15:47:32 +0000

    More improvements to intermag experiment

--- control_scripts/intermag.py
+++ control_scripts/intermag.py
@@ -36,71 +36,83 @@ import oomphpy
 import oomphpy.micromagnetics as mm
 
 
+def try_run(args, dt, dir_naming_args, root_outdir,
+             maxallowederror = 0.01, maxallowedangle = sp.pi/4):
 
-def locate_stable_point(args, refine, dt_guess, dir_naming_args,
-                        root_outdir):
+    args.update({'-dt' : dt})
+
+    # Try to run it
+    err_code, outdir = mm._run(args, root_outdir, dir_naming_args,
+                                quiet=True)
+
+
+    # Parse output files
+    data = mm.parse_run(outdir)
+    if data is not None:
+        errs = data['m_length_error_means']
+        assert errs[0] >= 0
+        maxerror = max(errs)
+
+        angles = data['max_angle_errors']
+        maxangle = max(angles)
+
+        maxtime = data['times'][-1]
+        nsteps = len(data['times'])
+
+    else:
+        maxerror = sp.inf
+        maxangle = sp.inf
+        maxtime = 0
+        nsteps = 0
+
+    success = (err_code == 0 and maxerror < maxallowederror
+                and maxangle < maxallowedangle)
+
+    if success:
+        mm.okprint("Succedded in", data['-outdir'])
+    else:
+        mm.badprint(pjoin(data['-outdir'], "stdout:1:1:"), "FAILED")
+
+    return success, data
+
+
+
+
+def locate_stable_point(args, refine, dt_guess, **kwargs):
 
     # Catch failures immediately
     if dt_guess is None:
         return None
 
-    maxallowederror = 0.01
-    maxallowedangle = sp.pi/4 # radians
+    # Otherwise start from the initial guess
+    else:
+        dt = dt_guess
 
-    dt = dt_guess
+
     success = False
 
     args.update({'-ref' : refine})
 
-    while (dt >= 1e-6):
+    # If it gets below this dt then give up
+    while not success and (dt >= 1e-6):
 
-        args.update({'-dt' : dt})
+        # Try a run
+        success, data = try_run(args, dt, **kwargs)
 
-        # Try to run it
-        err_code, outdir = mm._run(args, root_outdir, dir_naming_args,
-                                    quiet=True)
-
-
-        # Parse output files
-        data = mm.parse_run(outdir)
-        if data is not None:
-            errs = data['m_length_error_means']
-            assert errs[0] >= 0
-            maxerror = max(errs)
-
-            angles = data['max_angle_errors']
-            maxangle = max(angles)
-
-            maxtime = data['times'][-1]
-            nsteps = len(data['times'])
-
-        else:
-            maxerror = sp.inf
-            maxangle = sp.inf
-            maxtime = 0
-            nsteps = 0
-
-
-        # If it didn't crash then check output
-        if (err_code != 0
-            or maxerror > maxallowederror
-            or maxangle > maxallowedangle):
-            mm.badprint(pjoin(outdir, "stdout:1:1:"), "FAILED",
-                        maxerror, maxangle)
-
+        # On fail half the step and try again
+        if not success:
             dt = dt/2
 
-        else:
-            mm.okprint("Succedded in", os.path.relpath(outdir, root_outdir))
-            return dt
 
-
-    # if we get here then it totally failed
-    return None
+    if not success:
+        # if we get here then it totally failed
+        return None
+    else:
+        return dt
 
 
 def locate_stable_points(args, refines_list, dt_guess, dir_naming_args,
-                         root_outdir):
+                         root_outdir, nbisection=0):
 
     # Also name by refinement and dt
     results_naming_args = copy.copy(dir_naming_args)
@@ -111,34 +123,73 @@ def locate_stable_points(args, refines_list, dt_guess, dir_naming_args,
 
     print("writing results to", results_name)
 
-    # Run for easiest case
-    dt = locate_stable_point(args, refine=refines_list[0],
-                             dt_guess=dt_guess, dir_naming_args=dir_naming_args,
-                             root_outdir=root_outdir)
+    # # Run for easiest case
+    # dt = locate_stable_point(args, refine=refines_list[0],
+    #                          dt_guess=dt_guess, dir_naming_args=dir_naming_args,
+    #                          root_outdir=root_outdir)
 
-    # Write to file
-    with open(results_name, 'a') as result_file:
-        result_file.write(str(refines_list[0]) + " " + str(dt) + "\n")
+    # # Write to file
+    # with open(results_name, 'a') as result_file:
+    #     result_file.write(str(refines_list[0]) + " " + str(dt) + "\n")
 
-    dts = [dt]
+    dt_ranges = []
+    dt = dt_guess
 
     # And the rest
-    for ref in refines_list[1:]:
+    for ref in refines_list:
 
         # find stable point for this refine, use previous dt as initial guess
         dt = locate_stable_point(args,
                                  refine=ref,
-                                 dt_guess=dts[-1],
+                                 dt_guess=dt,
                                  dir_naming_args=dir_naming_args,
                                  root_outdir=root_outdir)
-        dts.append(dt)
 
-        # Write to file
+        # Refine for more accurate dt value using binary search
+        if dt < dt_guess:
+            dt_range = refine_stable_point(args, dt, 2*dt, nbisection,
+                                        dir_naming_args=dir_naming_args,
+                                        root_outdir=root_outdir)
+        else:
+            dt_range = (dt_guess, dt_guess)
+
+        dt_ranges.append(dt_range)
+
+        # Output
+        print("Final range", dt_range)
         with open(results_name, 'a') as result_file:
-            result_file.write(str(ref) + " " + str(dt) + "\n")
+            result_file.write(str(ref)
+                              + " " + str(dt_range[0])
+                              + " " + str(dt_range[1])
+                              + "\n")
+
+        # Next refinement should use the maximum possibly stable value as
+        # its initial dt guess
+        _, dt = dt_range
 
 
-    return dts
+
+
+    return dt_ranges
+
+
+def refine_stable_point(args, dt_min, dt_max, nrefines, **kwargs):
+
+    # Recursion base case
+    if nrefines == 0:
+        return dt_min, dt_max
+
+    print("searching in", dt_min, dt_max, "for", nrefines, "times")
+
+    # See if the middle dt is stable
+    dt_mid = (dt_min + dt_max)/2
+    success, data = try_run(args, dt_mid, **kwargs)
+
+    # Recurse with appropriate dt min/max and one less nrefine
+    if success:
+        return refine_stable_point(args, dt_mid, dt_max, nrefines-1, **kwargs)
+    else:
+        return refine_stable_point(args, dt_min, dt_mid, nrefines-1, **kwargs)
 
 
 def main():
@@ -152,6 +203,8 @@ def main():
     parser.add_argument('--serial', action='store_true',
                          help="Don't do parallel sweeps")
     parser.add_argument('--outdir', help='delete old data')
+    parser.add_argument('--quick', action='store_true',
+                        help='quick version of parameters for testing')
     args = parser.parse_args()
 
     if args.outdir is None:
@@ -159,46 +212,54 @@ def main():
 
     argsdict = {
                 '-driver' : 'll',
-                '-tmax' : 2,
+                '-tmax' : 4,
                 '-hlib-bem' : 0,
                 '-renormalise' : 0,
                 '-mesh' : ['ut_sphere'], # 'sq_square'],
-                '-ms-method' : ['implicit', 'disabled', 'decoupled'],
+                '-ms-method' : ['implicit', 'disabled', 'decoupled', 'sphere'],
                 '-solver' : 'som-gmres',
-                '-prec' : 'som-main-exact',
-                '-ts' : ['midpoint-bdf', 'rk2'],
+                '-prec' : 'som-main-ilu',
+                '-ts' : ['rk2', 'midpoint-bdf', 'euler'],
                 '-scale' : 2,
                 '-fd-jac' : True,
                 '-damping' : [1.0, 0.1, 0.01],
                 '-check-angles' : 1,
                 }
+    refines = [1, 2, 3, 4]
+
+    # Only do a couple of things for a test run
+    if args.quick:
+        argsdict['-ms-method'] = 'disabled'
+        argsdict['-damping'] = 1.0
+        argsdict['-ts'] = 'euler'
+        refines = [1, 2, 3]
 
 
     # The exact function to run
-    f = par(locate_stable_points, refines_list = [1, 2, 3, 4, 5],
-            dt_guess=0.1, dir_naming_args=mm.argdict_varying_args(argsdict),
-            root_outdir=args.outdir)
+    f = par(locate_stable_points,
+            refines_list = refines,
+            dt_guess=0.1,
+            dir_naming_args=mm.argdict_varying_args(argsdict),
+            root_outdir=args.outdir,
+            nbisection=2)
 
-    # Run for all combinations of args
+    # Create list of all possible combinations of args
     argsets = mm.product_of_argdict(argsdict)
 
-    # Remove an impossible combination
-    argsets = [a for a in argsets if not
-               (a['-ts'] == 'rk2' and a['-ms-method'] == 'implicit')]
+    # Filter out impossible combinations
+    def is_bad_combination(data):
+        if data['-ts'] == 'rk2' and data['-ms-method'] == 'implicit':
+            return True
+        elif data['-ts'] == 'euler' and data['-ms-method'] == 'implicit':
+            return True
+        else:
+            return False
+    argsets = [a for a in argsets if not is_bad_combination(a)]
+
 
     dts = list(mm.parallel_map(f, argsets, serial_mode=args.serial))
 
     print(dts)
-
-    # # Write to file
-    # with open(pjoin(root_outdir, 'results'), 'w') as result_file:
-    #     for r, dt in zip(refs, dts):
-    #         result_file.write(str(r) + " " + str(dt))
-
-    # plt.plot(refs, dts)
-    # plt.xlabel('refinement')
-    # plt.ylabel('max stable dt found')
-    # plt.show()
 
     return 0
 
