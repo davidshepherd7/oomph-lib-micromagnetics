2f09e1c9c2421908968d02c35f015153cf415885
David Shepherd <davidshepherd7@gmail.com>
2014-08-04 18:44:07 +0100

    More work on checking of llg residuals in python

--- etc/check_nodal_quadrature.py
+++ etc/check_nodal_quadrature.py
@@ -8,18 +8,33 @@ import copy
 
 import scipy as sp
 import scipy.linalg
+import scipy.integrate
+import scipy.optimize
 from scipy import sin, cos, sqrt, pi, dot, cross, array
+from scipy.optimize import newton
 
 from os.path import join as pjoin
 
 import functools as ft
 from functools import partial as par
 
+import itertools as it
+
+from pprint import pprint
+
 
 # ??ds shape functions should really be a property of nodes I think...
 
 # ??ds use factory to construct nodes with the expected properties?
 
+
+# # testing for element
+# s = sp.array([1, 0.5])
+# print([n.x for n in ele.nodes])
+# print([ele.shape(s, j) for j in ele.nodei()])
+# print([ele.dshapedx(s, j) for j in ele.nodei()])
+# print(ele.interpolate(s, lambda n:n.x))
+# print(ele.interpolate(s, lambda n:n.m))
 
 
 class Node(object):
@@ -35,10 +50,14 @@ class Element(object):
     def __init__(self):
         self.nodes = []
         self.el_dim = 0
+        self.quad = None
 
     # helpers
+    def nnode(self):
+        return len(self.nodes)
+
     def nodei(self):
-        return range(0, len(self.nodes))
+        return range(0, self.nnode())
 
     def dim():
         return len(self.nodes[0].x)
@@ -72,6 +91,9 @@ class Element(object):
     def dsdx(self, s):
         return sp.linalg.inv(self.dxds(s))
 
+    def J(self, s):
+        return sp.linalg.det(self.dxds(s))
+
     def interpolate(self, t, s, nodal_function):
         return sum([nodal_function(n) * self.shape(s, i)
                     for i, n in enumerate(self.nodes)])
@@ -80,12 +102,30 @@ class Element(object):
         return sum([sp.outer(nodal_function(n), self.dshapedx(s, i))
                     for i, n in enumerate(self.nodes)])
 
+    def interpolate_data(self, t, s, nodal_values):
+        return sum([nodal_values[i] * self.shape(s, i)
+                    for i, n in enumerate(self.nodes)])
+
+    def interpolate_data_dx(self, t, s, nodal_values):
+        return sum([sp.outer(nodal_values[i], self.dshapedx(s, i))
+                    for i, n in enumerate(self.nodes)])
+
 
 class TriElement(Element):
 
     def __init__(self, x0, x1, x2):
         self.nodes = [Node(x0), Node(x1), Node(x2)]
         self.el_dim = 2
+
+    def local_coordinate_of_node(self, j):
+        if j == 0:
+            return sp.array([1, 0])
+        elif j == 1:
+            return sp.array([0, 1])
+        elif j == 2:
+            return sp.array([0, 0])
+        else:
+            raise IndexError
 
 
     def shape(self, s, j):
@@ -119,18 +159,20 @@ class Quadrature(object):
         assert(len(self.weights) == len(self.knots))
 
 
-    def integrate(self, function, t, *arg_functions):
-        # eval each arg at each knot
-        arg_values = [[argf(t, k) for argf in arg_functions] for k in self.knots]
-
+    def integrate(self, function, p=False):
         # eval function at each knot
-        knot_values = sp.array([function(t, *kargs)
-                                for k, kargs in zip(self.knots, arg_values)])
+        knot_values = sp.array([function(s) for s in self.knots])
 
         # Add up and multiply by weights
-        return sp.dot(knot_values, self.weights)
+        thesum = 0.0
+        for v, w in zip(knot_values, self.weights):
+            thesum += v*w
+            if p:
+                print("integartion", v,w,thesum)
 
-# nodal_quadrature = quadrature(nodes, betas, integrand)
+
+        return thesum
+
 
 def m_initial(x):
     l = 50
@@ -146,79 +188,151 @@ def m_initial(x):
     return m
 
 
-def nodal_quadrature_fac(ele):
-    gaussquad = Quadrature([[1/6, 1/6], [2/3, 1/6], [1/6, 2/3]],
-                           [1/3, 1/3, 1/3])
+def m_initial_z(x):
+    m = array([0.5, 0.1, 1.0])
+    ml = sqrt(sp.dot(m,m))
+    m = m/ml
+    return m
 
+
+def nodal_quadrature_fac(ele):
     betas = []
     for i, n in enumerate(ele.nodes):
-        beta = gaussquad.integrate(lambda t,s: ele.shape(s, i),
-                                   0.0,
-                                   lambda t,s:s)
+        beta, _ = sp.integrate.dblquad(lambda x,y: ele.shape([x,y], i),
+                                        0, 1,
+                                        lambda x: 0,
+                                        lambda x: 1-x)
         betas.append(beta)
 
     return Quadrature([n.x for n in ele.nodes], betas)
 
 
+def skew(a):
+    result = sp.zeros((3,3))
+    result[0, 1] = -a[2];
+    result[0, 2] = a[1];
+    result[1, 0] = a[2];
+    result[1, 2] = -a[0];
+    result[2, 0] = -a[1];
+    result[2, 1] = a[0];
+
+    return result
+
+
+def integrand(t, x, mnext, mprev, dmdx, test, dtestdx, happ, p=False):
+
+    dampc = 0.5
+    dt = 0.1
+
+    # imr approximations
+    m = (mnext + mprev)/2
+    dmdt = (mnext - mprev)/dt
+
+     # Componentwise dot product of mxdmdx and dtestdx (pretending we
+    # have vector test functions)
+    mxdmdx = sp.dot(skew(m), dmdx)
+    exch = sp.array([sp.dot(r_mxdmdx, dtestdx) for r_mxdmdx in mxdmdx])
+
+    if p:
+        print(mnext, mprev)
+        a = ((mnext - mprev) / 0.1)*test + sp.dot(sp.cross((mnext + mprev) / 2, happ), test)
+        b = -0.5 * sp.cross((mnext + mprev) / 2, (mnext - mprev)/0.1) * test
+        print("*", a)
+        print("**", b)
+        print("dmdt =", dmdt)
+
+    return (sp.dot(dmdt, test)
+            + sp.dot(sp.cross(m, happ), test)
+            - dampc * sp.dot(sp.cross(m, dmdt), test)
+            - exch)
+
+
+def residual(ele, nodal_m_vals, time, happ, p=False):
+
+    # Calculate residuals for each test function
+    rs = []
+    for i_node in ele.nodei():
+
+        # Integrand as a function only of local coord
+        def integrand2(s):
+            x = ele.interpolate(time, s, lambda n:n.x)
+            mnext = ele.interpolate_data(time, s, nodal_m_vals)
+            mprev = ele.interpolate(time, s, lambda n:n.mprev)
+            dmdx = ele.interpolate_data_dx(time, s, nodal_m_vals)
+            test = ele.test(s, i_node)
+            dtestdx = ele.dtestdx(s, i_node)
+
+            return integrand(time, x, mnext, mprev, dmdx, test, dtestdx, happ, p)
+
+        rs.append(ele.quad.integrate(integrand2, p))
+
+    return sp.array(rs).flatten()
+
+
+def mlen(m):
+    return abs(1 - sqrt(sp.dot(m,m)))
+
+
+def output_solution(t, ele):
+    print("t =", t)
+    for n in ele.nodes:
+        print(n.x, n.m, n.mprev, mlen(n.m))
+    print()
+
+    return
+
+
+def initialise_solution(ele, func):
+    for n in ele.nodes:
+        n.mprev = copy.deepcopy(func(n.x))
+        n.m = copy.deepcopy(func(n.x))
 
 
 def main():
 
-    # create element
+    # create element and quadrature
     ele = TriElement([1,0], [0, 1], [0, 0])
+    ele.quad = nodal_quadrature_fac(ele)
 
     # Set initial conditions
-    for n in ele.nodes:
-        n.m_prev = m_initial(n.x)
-        n.m = m_initial(n.x)
 
+    # initialise_solution(ele, m_initial)
+    # happ = array([0,0,0])
 
-    # # testing
-    # s = sp.array([1, 0.5])
-    # print([n.x for n in ele.nodes])
-    # print([ele.shape(s, j) for j in ele.nodei()])
-    # print([ele.dshapedx(s, j) for j in ele.nodei()])
-    # print(ele.interpolate(s, lambda n:n.x))
-    # print(ele.interpolate(s, lambda n:n.m))
+    initialise_solution(ele, m_initial_z)
+    happ = array([0, 0, -1.1])
 
-    def integrand(t, x, m, dmdx, test, dtestdx):
-        return
+    dt = 0.1
+    t = 0.0
 
+    output_solution(t, ele)
 
-    def integrand(t, x, mnext, mprev, dmdx, test, dtestdx):
+    while t < 1.0:
 
-        dampc = 0.5
-        dt = 0.1
+        t = t + dt
 
-        # imr:
-        m = (mnext + mprev)/2
-        dmdt = (mnext - mprev)/dt
+        temp = sp.array([n.m for n in ele.nodes])
+        print(residual(ele, temp, happ=happ, time=t, p=True))
 
-        print(t,x,m, dmdt,dmdx)
-        print("*", test, dtestdx)
+        # minimise residual
+        curr_m = sp.array([[1,1,1] for n in ele.nodes])
+        out = sp.optimize.newton_krylov(lambda m: residual(ele, m,
+                                                     happ=happ,
+                                                     time=t),
+                                        copy.deepcopy(curr_m),
+                                        f_tol=1e-12,
+                                        rdiff=1e-14,
+                                        )
 
-        return (sp.dot(dmdt, test)
-                + dampc * sp.dot(sp.cross(m, dmdt), test)
-                - sp.dot(sp.cross(m, dmdx), dtestdx))
+        # Update m values
+        for new_m, n in zip(out, ele.nodes):
+            n.mprev = copy.deepcopy(n.m)
+            n.m = copy.deepcopy(new_m)
 
+            print(n.m - n.mprev)
 
-    # Create nodal quadrature
-    # ============================================================
-
-
-    quad = nodal_quadrature_fac(ele)
-    i_node = 0
-    quad.integrate(integrand, 0.0,
-                   par(ele.interpolate, nodal_function=lambda n:n.x),
-                   par(ele.interpolate, nodal_function=lambda n:n.m),
-                   par(ele.interpolate, nodal_function=lambda n:n.m_prev),
-                   par(ele.interpolate_dx, nodal_function=lambda n:n.m),
-                   lambda t,s: ele.test(s, i_node),
-                   lambda t,s: ele.dtestdx(s, i_node)
-                   )
-
-
-
+        # Print
+        output_solution(t, ele)
 
     return 0
 
